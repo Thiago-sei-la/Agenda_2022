19213e6b02e51a54b37a58119d3640db2639105c
Thiago-sei-la <102225029+Thiago-sei-la@users.noreply.github.com>
2022-04-20 10:57:28 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,92 +1,88 @@
 <?php
-  ob_start(); //armazena coisas em cache
-  session_start();//inicia sessão
-  if(isset($_SESSION['loginuser'])&&(isset($_SESSION['senhauser']))){
-   header("Location: home.php");
+  ob_start();
+  session_start();
+  if(isset($_SESSION['loginuser'])&&isset($_SESSION['senhauser'])){
+    header("Location: home.php");
   }
 ?>
 <!DOCTYPE html>
-<html lang="pt_br">
+<html lang="en">
+
 <head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Agenda JMF | Login</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Star Admin2 </title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="vendors/feather/feather.css">
+  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="vendors/typicons/typicons.css">
+  <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
+  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/favicon.png" />
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="../../index2.html"><b>Agenda</b>JMF</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Faça login para cadastrar os contatos</p>
 
-      <form action="" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Digite seu E-mail..." name="email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Digite sua senha..." name="senha">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          
-          <!-- /.col -->
-          <div class="col-12">
-            <button name="btnlogin" type="submit" class="btn btn-primary btn-block">Acessar Agenda</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <?php 
-      include_once('config/conexao2.php');
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="images/logo.svg" alt="logo">
+              </div>
+              <h4>Cadastre de clientes</h4>
+              <h6 class="fw-light">Faça login para continuar.</h6>
+              <form action="" method="post">
+                <div class="form-group">
+                  <input name="e-mail"type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="E-mail">
+                </div>
+                <div class="form-group">
+                  <input name="senha" type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Senha">
+                </div>
+                <div class="mt-3">
+                  <button name="btlogin" type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">ACESSAR O SISTEMA</button>
+                </div>
+                
+              </form>
+              <?php 
+      include_once('config/conect.php');
       if(isset($_GET['acao'])){
-          $acao = $_GET['acao'];
-          //0000000echo $acao;
-          if($acao=='negado'){
-            echo  '<br> <div class="alert alert-danger" role="alert"> Hey Jude </div>';
-          }elseif ($acao=='sair') {
-            echo '<br> <div class="alert alert-danger" role="alert"> Take a sad song and make it better </div>';
-          }
-      }
-      if(isset($_POST['btnlogin'])){
-        $login=$_POST['email'];
+        $acao = $_GET['acao'];
+        //0000000echo $acao;
+        if($acao=='negado'){
+          echo  '<br> <div class="alert alert-danger" role="alert"> Hey Jude </div>';
+        }elseif ($acao=='sair') {
+          echo '<br> <div class="alert alert-danger" role="alert"> Take a sad song and make it better </div>';
+        }
+    }
+      if(isset($_POST['btlogin'])){
+        $login=$_POST['e-mail'];
         $senha=base64_encode($_POST['senha']);
-        $select="SELECT * FROM tb_user WHERE email_user=:emailogin AND senha_user=:senhalogin";
+        $select="SELECT * FROM tb_cliente WHERE email_client=:emaillogin AND senha_client=:senhalogin";
         try{
         $resultlogin = $conect->prepare($select);
-        $resultlogin->bindParam(':emailogin',$login,PDO::PARAM_STR);
+        $resultlogin->bindParam(':emaillogin',$login,PDO::PARAM_STR);
         $resultlogin->bindParam(':senhalogin',$senha,PDO::PARAM_STR);
         $resultlogin->execute();
 
         $verificar = $resultlogin->rowCount();
         if($verificar>0){
-          $login=$_POST['email'];
+          $login=$_POST['e-mail'];
           $senha=$_POST['senha'];
           //cria session »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»boobs»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»x
           $_SESSION['loginuser'] = $login;
           $_SESSION['senhauser'] = $senha;
-          echo "ya";
-          header("Refresh: 1, home.php");
+          echo "yay";
+          header("Refresh: 2, home.php");
         }else{
           echo "nay";
         }
@@ -96,24 +92,28 @@
       }
       ?>
 
-      
-      <!-- /.social-auth-links -->
-
-      
-      <p style="margin-top:20px; text-align:center" class="mb-0">
-        <a href="registro.php" class="text-center">Cadastro para novo usuário</a>
-      </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
     </div>
-    <!-- /.login-card-body -->
+    <!-- page-body-wrapper ends -->
   </div>
-</div>
-<!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../../vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="../../vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="../../js/off-canvas.js"></script>
+  <script src="../../js/hoverable-collapse.js"></script>
+  <script src="../../js/template.js"></script>
+  <script src="../../js/settings.js"></script>
+  <script src="../../js/todolist.js"></script>
+  <!-- endinject -->
 </body>
+
 </html>
